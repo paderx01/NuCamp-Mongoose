2ddd5819f14270a1e002c3463c641138632ab278
Pader Xiong <paderx26@gmail.com>
2023-02-25 08:11:25 -0600

    Mongoose part 3

--- models/campsite.js
+++ models/campsite.js
@@ -2,6 +2,31 @@ const { SchemaTypeOptions } = require("mongoose");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const campsiteSchema = new Schema(
   {
     name: {
@@ -13,6 +38,7 @@ const campsiteSchema = new Schema(
       type: String,
       required: true,
     },
+    comments: [commentSchema],
   },
   {
     timestamps: true,
